97bee765242c78d978afd366dcbf15765a01a3d9
montecristo2022 <104501641+montecristo2022@users.noreply.github.com>
2023-06-01 18:18:20 +0300

    commit

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const app = require("./app");
 const mongoose = require("mongoose");
 
 const DB_HOST =
-  "mongodb+srv://montebet2020:Ok7akPE6uUnrv4zb@cluster0.n3m0qif.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://montebet2020:Ok7akPE6uUnrv4zb@cluster0.n3m0qif.mongodb.net/contacrs_reader?retryWrites=true&w=majority";
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
     app.listen(3000, () => {
-      console.log("Сервер запущен на порту 3000");
+      console.log("Сервер запущен на порту 3000!!!");
     });
   })
   .catch((error) => {
